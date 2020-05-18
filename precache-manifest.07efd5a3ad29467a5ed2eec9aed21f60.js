754e1293c05cff45bb3db57af9ff1dbf10ec05a9
Guilherme Vieira <guilhermevdm@gmail.com>
2020-05-18 01:17:52 -0300

    Updates

---
+++ precache-manifest.07efd5a3ad29467a5ed2eec9aed21f60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735aad731034870a76f8dc0c03f3fe79",
+    "url": "/github-clone/index.html"
+  },
+  {
+    "revision": "7ddd5ae9359486662acf",
+    "url": "/github-clone/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "aeef0a70c7f5d7ac1391",
+    "url": "/github-clone/static/js/2.c33cacbf.chunk.js"
+  },
+  {
+    "revision": "89d3385f7d39af76bd84ec1c3fe43df2",
+    "url": "/github-clone/static/js/2.c33cacbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ddd5ae9359486662acf",
+    "url": "/github-clone/static/js/main.bca87558.chunk.js"
+  },
+  {
+    "revision": "45bd79b46d2dd9192b21",
+    "url": "/github-clone/static/js/runtime-main.a001f119.js"
+  },
+  {
+    "revision": "cddd8090c17ccebdb948614c96c31a38",
+    "url": "/github-clone/static/media/caixa.cddd8090.svg"
+  },
+  {
+    "revision": "364b6f4a4c3d5e427146468d5ceab0a8",
+    "url": "/github-clone/static/media/github-logo-white.364b6f4a.svg"
+  }
+]);
